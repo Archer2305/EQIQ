fc889a080b6cece550b0f409cea440698d32c852
BuildTools <unconfigured@null.spigotmc.org>
2023-01-09 14:29:56 -0800

    changes 1/9

--- MonkE2/src/subsytems/shooter.cpp
+++ MonkE2/src/subsytems/shooter.cpp
@@ -1,24 +1,24 @@
 #include "main.h"
 #include "shooter.hpp"
 bool toggle=false;
-bool lock=false;
+bool lock=true;
 Motor flywheel(10,false,AbstractMotor::gearset::blue,AbstractMotor::encoderUnits::degrees);
 void updateShooter(){
 
-if(controller.getDigital(ControllerDigital::R2) == 1){
-   if(!lock){
-        toggle=!toggle;
-      }
-      lock=true;
-        flywheel.moveVoltage(-12000);
-      }
-      if(controller.getDigital(ControllerDigital::R2)==0){
-        lock=false;
-      }
-      if(toggle==false){
-         flywheel.moveVelocity(0);
-      }
-  
+  if(controller.getDigital(ControllerDigital::L2) == 1){
+    if(lock){
+      toggle = !toggle;
+      lock = false;
+    }
+  }
+  if(toggle){
+    flywheel.moveVelocity(-360);
+  }else{
+    flywheel.moveVelocity(0);
+  }
+  if(controller.getDigital(ControllerDigital::L2) == 0){
+    lock = true;
+  }
 }
  /*
  
